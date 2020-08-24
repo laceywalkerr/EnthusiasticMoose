581c04ea158924d9748d4a5a420a7c5ae86d8071
Lacey Walker <bluelarksong@gmail.com>
2020-08-23 21:58:34 -0500

    message now works, forgot $ symbol first run through

--- Program.cs
+++ Program.cs
@@ -48,7 +48,7 @@ namespace EnthusiasticMoose
         }
         static void MooseSays(string message)
         {
-            Console.WriteLine(@"
+            Console.WriteLine($@"
                                     _.--^^^--,
                                     .'          `\
 .-^^^^^^-.                      .'              |
